88ca54ca8d3cdae2a57dcfebc886fdae2883beca
Bhagirath Saxena <72344025+rix4uni@users.noreply.github.com>
2024-10-13 18:31:30 +0530

    Added 2008

---
+++ 2008/CVE-2008-7220/demo.php
@@ -0,0 +1,6 @@
+<?php
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers:x-prototype-version");
+header("Access-Control-Allow-Headers:x-requested-with");
+header("Content-type:text/javascript");
+echo "alert(\"hello world!\")";
